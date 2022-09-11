938a9fa27ce16f47aeb032c497fc6ddc28073e10
imkilwon <yuvwz_coding@naver.com>
2022-09-11 15:55:20 +0900

    project 생성
    firebase 연결하기

---
+++ note_market/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  if (kIsWeb) {
+    //웹으로 접속하였을 때 웹의 정보를 전달하는 용도
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyCEayBAxfwtN3o8W5SlFvmzKWMyh03zVr8",
+            authDomain: "notemarket-bac09.firebaseapp.com",
+            projectId: "notemarket-bac09",
+            storageBucket: "notemarket-bac09.appspot.com",
+            messagingSenderId: "61803631515",
+            appId: "1:61803631515:web:05e6fc1bdac0d39549073e",
+            measurementId: "G-5QXT06ETLN"));
+  }else{
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+  }
+  runApp(const NoteMarket());
+}
+
+class NoteMarket extends StatelessWidget {
+  const NoteMarket({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
